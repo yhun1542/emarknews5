3e014ef36db86e0aa46718d381110819ae4d9177
DevOps Deploy <deploy@emarknews.com>
2025-08-16 20:49:07 -0400

    undici fix: Downgrade + polyfill for File error

--- backend_upgraded_hybrid_final2.js
+++ backend_upgraded_hybrid_final2.js
@@ -1211,3 +1211,5 @@ process.on("unhandledRejection", (err) => console.error("Unhandled Rejection:", 
 process.on("uncaughtException", (err) => console.error("Uncaught Exception:", err));
 process.on("unhandledRejection", (err) => console.error("Unhandled Rejection:", err));
 process.on("uncaughtException", (err) => console.error("Uncaught Exception:", err));
+process.on("unhandledRejection", (err) => console.error(err));
+process.on("uncaughtException", (err) => console.error(err));
